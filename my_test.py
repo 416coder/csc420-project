f38cef6609db3e95e156a9de467083591d300d31
Sriram Srirangam <sriram.srirangam@mail.utoronto.ca>
2019-04-13 16:05:40 -0400

    Added solution printing code

--- my_test.py
+++ my_test.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 from skimage import transform as tf
 from scipy.cluster.vq import vq, kmeans, whiten
 from rubik_solver import utils
+from sty import fg, bg, ef, rs, RgbFg
 
 # plt.subplot(121),plt.imshow(img,cmap = 'gray')
 # plt.title('Original Image'), plt.xticks([]), plt.yticks([])
@@ -13,7 +14,7 @@ from rubik_solver import utils
 
 sides = ["top", "left", "front", "right", "back", "bottom"]
 
-def get_corners(img_name):
+def get_corners_canny(img_name):
     img = cv2.imread(img_name, 0)
     edges = cv2.Canny(img,300,300)
 
@@ -29,13 +30,13 @@ def get_corners(img_name):
     x = [x_min, x_max, x_max, x_min]
     y = [y_min, y_min, y_max, y_max]
 
+    plt.figure()
+    plt.imshow(cv2.cvtColor(cv2.imread(img_name), cv2.COLOR_BGR2RGB))
+    plt.plot(y + y[:1], x + x[:1], 'r')
+    plt.show()
+
     return x, y
 
-
-# plt.figure()
-# plt.imshow(og_img)
-# plt.plot((y_min, y_min, y_max, y_max, y_min), (x_min, x_max, x_max, x_min, x_min), 'r')
-# plt.show()
 
 # 4 pixels per mm
 f = 4
@@ -110,20 +111,64 @@ def plot_cube_layout(all_colors, with_spaces=True):
 
     plt.figure()
     plt.imshow(np.ndarray.astype(show_img, np.uint8))
+    plt.title("Cube Layout")
     plt.show()
+
+def print_solution_instructions(solution, face_center_labels):
+    print("=" * 10)
+    print("SOLUTION")
+    print("=" * 10)
+    for move in solution:
+        f = move[0]
+        if f == 'L' or f == 'F' or f == 'R' or f == 'B':
+            top_label = face_center_labels[0]
+        elif f == 'U':
+            top_label = face_center_labels[4]
+        else:
+            top_label = face_center_labels[2]
+
+        if f == 'U': front_label = face_center_labels[0]
+        elif f == 'L': front_label = face_center_labels[1]
+        elif f == 'F': front_label = face_center_labels[2]
+        elif f == 'R': front_label = face_center_labels[3]
+        elif f == 'B': front_label = face_center_labels[4]
+        else: front_label = face_center_labels[5]
+
+        if len(move) == 1:
+            direction = "clockwise"
+        elif move[1] == '\'':
+            direction = "counterclockwise"
+        else:
+            direction = "clockwise twice"
+
+        print("Orient the cube so that center " + str(front_label) + " is facing you and center " + str(top_label) + " is on top")
+        print("Turn the side of the cube facing you " + direction)
+        input("Press the ENTER key to show the next step")
+        print("=" * 10)
+
+    print("Congratulations, you just solved the cube!")
 
 
 # Find the average RGB value for each square on the cube
 all_colors = []
 for side in sides:
-    img_name = "test_images/" + side + "2.jpg"
-    x, y = get_corners(img_name)
+    img_name = "test_images/" + side + "3.jpg"
+    x, y = get_corners_canny(img_name)
     warped = compute_homography(img_name, x, y)
     all_colors += get_face_colors(warped)
 
-# Performs k-Means clustering on the RGB values and give them
+    # plt.figure()
+    # plt.imshow(warped)
+    # plt.plot(x2 + x2[:1], y2 + y2[:1], 'r-')
+    # plt.plot([19*f, 19*f], [1, 57*f], 'r-')
+    # plt.plot([38*f, 38*f], [1, 57*f], 'r-')
+    # plt.plot([1, 57*f], [19*f, 19*f], 'r-')
+    # plt.plot([1, 57*f], [38*f, 38*f], 'r-')
+    # plt.show()
+
+# Performs k-means clustering on the RGB values and give them
 # each one of k labels, where k = 6
-centers, distortion = kmeans(all_colors, k_or_guess = 6)
+centers, _ = kmeans(all_colors, k_or_guess = 6)
 labels = []
 for color in all_colors:
     dists = []
@@ -153,13 +198,19 @@ for label in labels:
     input_str += labels_to_colors[label]
 
 # Call the solving algorithm
-print(utils.solve(input_str, 'Kociemba'))
+solution = utils.solve(input_str, 'Kociemba')
 
-# plt.figure()
-# plt.imshow(warped)
-# plt.plot(x2 + x2[:1], y2 + y2[:1], 'r-')
-# plt.plot([19*f, 19*f], [1, 57*f], 'r-')
-# plt.plot([38*f, 38*f], [1, 57*f], 'r-')
-# plt.plot([1, 57*f], [19*f, 19*f], 'r-')
-# plt.plot([1, 57*f], [38*f, 38*f], 'r-')
-# plt.show()
+# Plot reference diagram
+plt.figure()
+ref = np.zeros((6, 1, 3))
+for i in range(len(centers)):
+    ref[i] = centers[i]
+plt.imshow(np.ndarray.astype(ref, np.uint8))
+plt.title("Reference Diagram for Solution")
+plt.show(block=False)
+
+# Output the solution step-by-step
+face_center_labels = [labels[9*i+4] for i in range(6)]
+print(solution)
+solution_strs = [str(move) for move in solution]
+print_solution_instructions(solution_strs, face_center_labels)
